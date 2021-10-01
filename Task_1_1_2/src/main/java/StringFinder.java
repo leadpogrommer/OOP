f5573bb809e0a7e3d8195cc0580d9e1a2e58f10a
leadpogrommer <ilyamerzlakov@gmail.com>
2021-10-01 15:48:47 +0700

    rewrite string finder

--- Task_1_1_2/src/main/java/StringFinder.java
+++ Task_1_1_2/src/main/java/StringFinder.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Class with cool method for finding strings in files
@@ -10,17 +11,59 @@ import java.util.ArrayList;
 public class StringFinder {
     /**
      * Finds all occurrences  of string in files
-     * @param filepath Path to the file where we should search for strinf
-     * @param substring String that should be searched
-     * @return ArrayList with string positions
-     * @throws IOException Because IDEA wants me to write 'throws'
+     *
+     * @param filepath Path to the file where we should search for string
+     * @param needle   String that should be searched
+     * @return List with string positions
+     * @throws IOException If file does not exist
      */
-    public static ArrayList<Integer> findSubstring(String filepath, String substring) throws IOException {
-        var file = RandomAccessFile()
+    public static List<Integer> findSubstring(String filepath, String needle) throws IOException {
+        var rdr = new BufferedReader(new FileReader(filepath));
 
+
+        var m = needle.length();
+        var s = needle.toCharArray();
+        var suffshift = new int[m + 1];
+        Arrays.fill(suffshift, m);
+        var z = new int[m];
+        for (int j = 1, maxZidx = 0, maxZ = 0; j < m; j++) {
+            if (j <= maxZ) z[j] = Math.min(maxZ - j + 1, z[j - maxZidx]);
+            while (j + z[j] < m && s[m - 1 - z[j]] == s[m - 1 - (j + z[j])]) z[j]++;
+            if (j + z[j] - 1 > maxZ) {
+                maxZidx = j;
+                maxZ = j + z[j] - 1;
+            }
+        }
+        for (int j = m - 1; j > 0; j--) suffshift[m - z[j]] = j;
+        for (int j = 1, r = 0; j <= m - 1; j++) {
+            if (j + z[j] == m) {
+                for (; r <= j; r++) {
+                    if (suffshift[r] == m) suffshift[r] = j;
+                }
+            }
+        }
+
+        int j, bound = 0;
+        int i = 0;
+
+        var cbuf = new char[m];
         var ret = new ArrayList<Integer>();
 
-
+        while (true) {
+            rdr.mark(2 * m);
+            if (rdr.read(cbuf, 0, m) != m) break;
+            for (j = m - 1; j >= bound && s[j] == cbuf[j]; ) j--;
+            if (j < bound) {
+                ret.add(i);
+                bound = m - suffshift[0];
+                j = -1;
+            } else {
+                bound = 0;
+            }
+            rdr.reset();
+            if (rdr.skip(suffshift[j + 1]) != suffshift[j + 1]) break;
+            i += suffshift[j + 1];
+        }
 
         return ret;
     }
--- Task_1_1_2/src/test/java/StringFinderTest.java
+++ Task_1_1_2/src/test/java/StringFinderTest.java
@@ -1,52 +1,51 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class StringFinderTest {
-    /**
-     * @param content File content
-     * @return Absolute path of created file
-     * @throws IOException Because IDEA
-     */
-    static String createTempFile(String content) throws IOException {
-        var t = File.createTempFile("___", ".txt");
-        t.deleteOnExit();
-
-        var writer = new FileWriter(t);
-        writer.write(content);
-
-        writer.close();
-
-        return t.getAbsolutePath();
+    static Stream<Arguments> getStrings() {
+        return Stream.of(
+                Arguments.of("Hello, world!", "Hello", new int[]{0}),
+                Arguments.of("Hello, world!", "World", new int[]{}),
+                Arguments.of("Hello, world!", "world", new int[]{7}),
+                Arguments.of("aaabb", "aabb", new int[]{1}),
+                Arguments.of("Some cool multiline text with\nsome cool words", "cool", new int[]{5, 35}),
+                Arguments.of("", "a", new int[]{}),
+                Arguments.of("cc", "cc", new int[]{0})
+        );
     }
 
 
     /**
-     * Helper function because JUnit cannot compare collections
-     * @param res Return value of StringFinder.findSubstring()
-     * @param nums Expected indexes
+     * @param haystack String to search in
+     * @param needle   String to search for
+     * @param nums     Array of expected indexes
+     * @throws IOException On temporary file creation error
      */
-    static void assertResult(ArrayList<Integer> res, Integer... nums) {
-        assert (res.equals(Arrays.asList(nums)));
-    }
+    @ParameterizedTest(name = "[{index}] Search {1} in {0}")
+    @MethodSource("getStrings")
+    void findSubstring(String haystack, String needle, int[] nums) throws IOException {
+        File f = null;
+        try {
+            f = File.createTempFile("---", "---");
+            var writer = new FileWriter(f);
+            writer.write(haystack);
 
+            writer.close();
 
-    @Test
-    void findSubstring() throws IOException {
-        var filename = createTempFile("Hello, world!");
-        assertResult(StringFinder.findSubstring(filename, "Hello"), 0);
-        assertResult(StringFinder.findSubstring(filename, "World"));
-        assertResult(StringFinder.findSubstring(filename, "world"), 7);
+            var res = StringFinder.findSubstring(f.getPath(), needle).stream().mapToInt(Integer::intValue).toArray();
+            assertArrayEquals(nums, res);
 
-        filename = createTempFile("Some cool multiline text with\nsome cool words");
-        assertResult(StringFinder.findSubstring(filename, "cool"), 5, 35);
-
-
-        filename = createTempFile("aaabb");
-        assertResult(StringFinder.findSubstring(filename, "aabb"), 1);
+        } finally {
+            if (f != null) //noinspection ResultOfMethodCallIgnored
+                f.delete();
+        }
     }
 }
