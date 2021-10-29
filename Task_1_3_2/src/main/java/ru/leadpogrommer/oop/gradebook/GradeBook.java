d58f8ffad62e26719ea5586859ccbd0c0f5eb506
leadpogrommer <ilyamerzlakov@gmail.com>
2021-10-29 15:30:48 +0700

    fixes for gradebook

--- Task_1_3_2/src/main/java/ru/leadpogrommer/oop/gradebook/GradeBook.java
+++ Task_1_3_2/src/main/java/ru/leadpogrommer/oop/gradebook/GradeBook.java
@@ -1,66 +1,49 @@
 package ru.leadpogrommer.oop.gradebook;
 
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class GradeBook {
-    private final Map<String, List<Integer>> subjects = new HashMap<>();
-    private final int numSemesters;
+    private final List<Map<String, Integer>> grades;
     Integer qualificationWorkMark = null;
 
-    GradeBook(int numSemesters){
-        this.numSemesters = numSemesters;
+
+    GradeBook(int numSemesters) {
+        grades = IntStream.range(0, numSemesters).boxed().map((i) -> new HashMap<String, Integer>()).collect(Collectors.toList());
     }
 
-    float averageGrade(){
-       int numGrades = 0;
-       float sum = 0;
-       for(var subject: subjects.values()){
-           for(int i = 0; i < numSemesters; i++){
-               if(subject.get(i) != null){
-                   numGrades++;
-                   sum += subject.get(i);
-               }
-           }
-       }
-       return sum/numGrades;
+    double averageGrade() {
+        return grades.stream().flatMap((semester) -> semester.values().stream()).mapToDouble(Double::valueOf).average().orElse(Double.NaN);
+
     }
 
-    boolean hasBigScholarship(int semester){
-        for(var subject: subjects.values()){
-            if(subject.get(semester) != null && subject.get(semester) != 5)return false;
-        }
-        return true;
+    boolean hasBigScholarship(int semester) {
+        return grades.get(convertSemester(semester)).values().stream().noneMatch((i) -> i != 5);
     }
 
-    boolean canGetRedDiploma(){
-        var lastMarkCount = 0.;
-        var lastMarkExcCount = 0.;
-
-        if(qualificationWorkMark == null || qualificationWorkMark != 5) return false;
-
-        for(var subject: subjects.values()){
-            for(var i = numSemesters -1; i >= 0; i--){
-                if(subject.get(i) != null){
-                    if(subject.get(i) < 4)return false;
-                    lastMarkCount++;
-                    if(subject.get(i) == 5)lastMarkExcCount++;
-                }
-            }
-        }
-
-        return !(lastMarkExcCount / lastMarkCount < 0.75);
+    boolean canGetRedDiploma() {
+        if (grades.stream().flatMap((semester) -> semester.values().stream()).anyMatch((i) -> i <= 3)) return false;
+        if (averageGrade() < 4.75) return false;
+        return qualificationWorkMark != null && qualificationWorkMark == 5;
     }
 
-    GradeBook addSubject(String name, Integer ...grades){
-        if(grades.length != numSemesters)throw new IllegalArgumentException("Grades count does not math number of semesters");
-        subjects.put(name, Arrays.asList(grades));
+    GradeBook setGrade(int semester, String name, int grade) {
+        grades.get(convertSemester(semester)).put(name, grade);
         return this;
     }
 
-    GradeBook setQualificationMark(Integer m){
+    GradeBook setQualificationMark(Integer m) {
         this.qualificationWorkMark = m;
         return this;
+    }
+
+    private int convertSemester(int semester) {
+        if (semester < 1 || semester > grades.size()) throw new IllegalArgumentException();
+        return semester - 1;
     }
 
 }
--- Task_1_3_2/src/test/java/ru/leadpogrommer/oop/gradebook/GradeBookTest.java
+++ Task_1_3_2/src/test/java/ru/leadpogrommer/oop/gradebook/GradeBookTest.java
@@ -6,18 +6,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GradeBookTest {
     @Test
-    void myData(){
+    void myData() {
         var gb = new GradeBook(4)
-                .addSubject("Матан", 4, 5, null, null)
-                .addSubject("Матлог", 5, 5, null, null)
-                .addSubject("История", 5, null, null, null)
-                .addSubject("ОКР", 5, null, null, null)
-                .addSubject("Декларативка", 5, 5, null, null)
-                .addSubject("Императивка", 5, 5, null, null)
-                .addSubject("Хоцкина", 5, 4, null, null)
-                .addSubject("ЦП", null, 5, null, null);
+                .setGrade(1, "Матан", 4)
+                .setGrade(1, "Дискретка", 5)
+                .setGrade(1, "Декларативка", 5)
+                .setGrade(1, "Императивка", 5)
+                .setGrade(1, "История", 5)
+                .setGrade(1, "ОКР", 5)
+                .setGrade(2, "Дискретка", 5)
+                .setGrade(2, "Декларативка", 5)
+                .setGrade(2, "Императивка", 5)
+                .setGrade(2, "Хоцкина", 4)
+                .setGrade(2, "Иртегов-бог", 5);
+
         assertFalse(gb.canGetRedDiploma());
         assertFalse(gb.hasBigScholarship(1));
         assertEquals(gb.averageGrade(), 4.7, 0.2);
+    }
+
+    @Test
+    void canGetRedDiploma() {
+        var gb = new GradeBook(1)
+                .setGrade(1, "1", 5)
+                .setGrade(1, "2", 5);
+        assertFalse(gb.canGetRedDiploma());
+        gb.setQualificationMark(4);
+        assertFalse(gb.canGetRedDiploma());
+        gb.setQualificationMark(5);
+        assertTrue(gb.canGetRedDiploma());
+        gb.setGrade(1, "1", 4);
+        assertFalse(gb.canGetRedDiploma());
+    }
+
+    @Test
+    void hasBigScholarship() {
+        var gb = new GradeBook(2)
+                .setGrade(1, "1", 5)
+                .setGrade(2, "1", 4);
+        assertFalse(gb.hasBigScholarship(2));
+        assertTrue(gb.hasBigScholarship(1));
+    }
+
+    @Test
+    void averageGrade() {
+        var gb = new GradeBook(2)
+                .setGrade(1, "1", 5)
+                .setGrade(1, "2", 3)
+                .setGrade(2, "1", 4);
+        assertEquals(4, gb.averageGrade(), 0.1);
+    }
+
+    @Test
+    void validateSemester() {
+        var gb = new GradeBook(2);
+        assertThrows(IllegalArgumentException.class, () -> gb.setGrade(0, "1", 5));
+        assertThrows(IllegalArgumentException.class, () -> gb.setGrade(3, "1", 5));
+        assertThrows(IllegalArgumentException.class, () -> gb.hasBigScholarship(-1));
     }
 }
