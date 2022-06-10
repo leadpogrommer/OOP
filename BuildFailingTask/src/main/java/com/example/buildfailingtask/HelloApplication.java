271b64432bb0acc4aa97b4e42a2a45e9e8738b92
leadpogrommer <ilyamerzlakov@gmail.com>
2022-06-10 15:32:56 +0700

    add failing tasks to test DSL

---
+++ BuildFailingTask/src/main/java/com/example/buildfailingtask/HelloApplication.java
@@ -0,0 +1,7 @@
+package com.example.buildfailingtask;
+
+class Main{
+    public static void main(String[] args) {
+        System.out.prxhdtrjintln("hello");
+    }
+}
---
+++ TestFailingTask/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+
+    int fun(){
+        return 2+2;
+    }
+}
